e41f049dd078741ed12ee4be629b8fcd5c77a6de
Nott <nott@163.com>
2024-05-24 10:58:18 +0800

    Initial commit

---
+++ hutu-web/admin/src/main/java/org/nott/AdminApplication.java
@@ -0,0 +1,10 @@
+package org.nott;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "org.nott")
+public class AdminApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# 糊涂点餐
+一个基于springboot2+mybatis-plus的点餐系统
+
+## 模块
+
+```
+├─hutu-web # web模块（api+管理端）
+├─hutu-model # 实体
+├─hutu-external # 外部系统api
+├─hutu-common # 通用模块
+```
+1
