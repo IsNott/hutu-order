95beb5272e2ce219c1786e9c7a03fdf74d1e6582
Nott <nott@163.com>
2024-06-24 14:26:27 +0800

    feat: api模块引入security模块，添加鉴权interceptor加载开关

--- hutu-security/src/main/java/org/nott/security/configuration/InterceptorConfiguration.java
+++ hutu-security/src/main/java/org/nott/security/configuration/InterceptorConfiguration.java
@@ -2,6 +2,7 @@ package org.nott.security.configuration;
 
 import org.nott.security.interceptor.AuthorizationInterceptor;
 import org.nott.security.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,12 +15,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class InterceptorConfiguration {
 
+    @Value("${interceptor.authorize}")
+    private boolean needAuthorize;
+
     @Bean
     WebMvcConfigurer createWebMvcConfigurer() {
         return new WebMvcConfigurer() {
             public void addInterceptors(InterceptorRegistry registry) {
                 registry.addInterceptor(new LoginInterceptor());
-                registry.addInterceptor(new AuthorizationInterceptor());
+                if(needAuthorize){
+                    registry.addInterceptor(new AuthorizationInterceptor());
+                }
             }
         };
     }
